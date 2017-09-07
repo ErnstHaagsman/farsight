a22374e8ccd12bed679c737bf01fd2d1748f9d16
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-09-07 16:34:11 +0200

    Configure database access

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,18 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from views.api_v1 import api
 
 app = Flask(__name__)
+
+try:
+    database_uri = os.environ['DATABASE_URI']
+except KeyError:
+    print("Please set the DATABASE_URI environment variable")
+    exit(1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+db = SQLAlchemy(app)
 
 app.register_blueprint(api, url_prefix='/api/v1')
 
